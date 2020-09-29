3a9bf58ba9d48d0a6e1b0a60980a6b216d6cbe48
nat <natalia@camba.coop>
2020-09-28 21:02:58 -0300

    update footer

--- src/Body.js
+++ src/Body.js
@@ -6,24 +6,64 @@ import './App.css';
 function Body() {
   return (
     <div>
-      <Experience />
-      <Skills />
       <div className="App-body header-elem-box">
         <div className="header-elem-box">
           <p className="title">
-            <strong>&gt;</strong> experience
+            <strong>&gt;</strong> about me
           </p>
-          <p className="exp-item">
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          <p className="exp-item text-console padding-small">
+            I've been a backend developer programming in Django and Symfony and I've had fun learning Node and React this year.
           </p>
-          <p className="exp-item">
-          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+          <p className="exp-item text-console padding-small">
+            I think collective projects are a wonderful way to work in groups of people where we can share experiences and knowledge.
           </p>
-          <p className="exp-item">
-          "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
+          <p className="exp-item text-console padding-small">
+            I'm also interested in art, free software & technical challenges. That's why I'm playing with Hydra, GTP-2 and other art-tech tools.
           </p>
         </div>
       </div>
+      <Experience />
+      <Skills />
+      <section
+        className="background-gray-3"
+        alt="Frontend"
+      >
+        <div className="padding-large column">
+          <p className="color-white">&gt; share with me new funny projects, tech challenges & new knowledges :)</p>
+          <a
+            className="color-white contact-list-item-centered"
+            href="https://www.linkedin.com/in/natalia-suarez-dev/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            + linkedin
+          </a>
+          <a
+            className="color-white contact-list-item-centered"
+            href="https://github.com/NataliaSuarez"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            + github
+          </a>
+          <a
+            className="color-white contact-list-item-centered"
+            href="https://twitter.com/natsuarez42"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            + twitter
+          </a>
+          <a
+            className="color-white contact-list-item-centered"
+            href="mailto:suarez.nataliam@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            + nat@dev
+          </a>
+        </div>
+      </section>
     </div>
   )
 }
--- src/Header.js
+++ src/Header.js
@@ -48,13 +48,7 @@ function Header() {
           nat | fullstack developer
         </p>
         <p className="description">
-          I've been a backend developer programming in Django and Symfony and I've had fun learning Node and React this year.
-        </p>
-        <p className="description">
-          I think collective projects are a wonderful way to work in groups of people where we can share experiences and knowledge.
-        </p>
-        <p className="description"> 
-          I'm also interested in art, free software & technical challenges. That's why I'm playing with Hydra, GTP-2 and other art-tech tools. 
+          Resume project develop with React. 
         </p>
       </div>
     </div>
