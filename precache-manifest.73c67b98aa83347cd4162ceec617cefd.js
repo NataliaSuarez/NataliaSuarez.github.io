8433a57b70cec1bac977dada9bf9bf61422636d6
nat <natalia@camba.coop>
2020-09-28 21:21:08 -0300

    Updates

---
+++ precache-manifest.73c67b98aa83347cd4162ceec617cefd.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef22eb88bb8d5f483758c3a085535141",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7a1f7ac20f1aa66546d",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "318f8c813fde18433936",
+    "url": "/static/css/main.d64e9409.chunk.css"
+  },
+  {
+    "revision": "c7a1f7ac20f1aa66546d",
+    "url": "/static/js/2.f1a26180.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.f1a26180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "318f8c813fde18433936",
+    "url": "/static/js/main.4589c773.chunk.js"
+  },
+  {
+    "revision": "73a4f215652a07459b34",
+    "url": "/static/js/runtime-main.3bbf3189.js"
+  },
+  {
+    "revision": "7150fccb26054f1dc6b38b0c7ea86187",
+    "url": "/static/media/backend.7150fccb.svg"
+  },
+  {
+    "revision": "1189846eb302ed8e36918605dac967e1",
+    "url": "/static/media/bootstrap.1189846e.svg"
+  },
+  {
+    "revision": "02681843cead7a862ae36841dc8ef322",
+    "url": "/static/media/creatividad.02681843.svg"
+  },
+  {
+    "revision": "8b309ee92e5c3f37572eaba0dc76a510",
+    "url": "/static/media/django.8b309ee9.svg"
+  },
+  {
+    "revision": "b23dc2ee32d2f824cff5f6016c4acd7b",
+    "url": "/static/media/docker.b23dc2ee.svg"
+  },
+  {
+    "revision": "21d7be19601b239ed62e61511a3ad64e",
+    "url": "/static/media/frontend.21d7be19.svg"
+  },
+  {
+    "revision": "2c185be14f7ee1df8772d002390845ec",
+    "url": "/static/media/html-5.2c185be1.svg"
+  },
+  {
+    "revision": "a17e7facb73aacc470f494b5d5708e43",
+    "url": "/static/media/mongo.a17e7fac.svg"
+  },
+  {
+    "revision": "084b5fed6ffe5c51175a2271ca8794d0",
+    "url": "/static/media/nodejs.084b5fed.svg"
+  },
+  {
+    "revision": "4f214e149d37c4b0a604c861966dc85d",
+    "url": "/static/media/php.4f214e14.svg"
+  },
+  {
+    "revision": "9c5826cbd9489a39c9a8787d5611fdc9",
+    "url": "/static/media/plataformas-online.9c5826cb.svg"
+  },
+  {
+    "revision": "e571c9c7c80c18655e7171bf6d7374a4",
+    "url": "/static/media/posgresql.e571c9c7.png"
+  },
+  {
+    "revision": "98d19974bf08af802ba39088e71d1878",
+    "url": "/static/media/python.98d19974.svg"
+  },
+  {
+    "revision": "a7212ed4c02ca28a98c310a947c21998",
+    "url": "/static/media/react.a7212ed4.png"
+  },
+  {
+    "revision": "cab3de2d8e86c7952f25a5b6786a1107",
+    "url": "/static/media/ui-ux-design.cab3de2d.svg"
+  },
+  {
+    "revision": "bdaff7a45127d05a3e2015cdb4777091",
+    "url": "/static/media/vue.bdaff7a4.webp"
+  }
+]);
