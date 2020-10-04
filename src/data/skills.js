fde65216315e2f12543b95cdb36b466e17d471f6
nat <natalia@camba.coop>
2020-10-04 15:27:02 -0300

    add skills radar with fixed styles

---
+++ src/data/skills.js
@@ -0,0 +1,75 @@
+const backend = [
+  {
+    data: {
+      php: 0.7,
+      symfony: .8,
+      laravel: .5,
+      java: 0.4,
+      kotlin: 0.5,
+      node: 0.67,
+      feathers: 0.6,
+      python: 0.8,
+      django: .8,
+    },
+    meta: { color: '#00b92e' }
+  },
+]
+const backendCaptions = {
+  php: 'PHP',
+  symfony: 'Symfony',
+  laravel: 'Laravel',
+  java: 'Java',
+  kotlin: 'Kotlin',
+  node: 'Node',
+  feathers: 'Feathers',
+  python: 'Python',
+  django: 'Django',
+};
+
+const frontend = [ 
+  {
+    data: {
+      react: 0.7,
+      vue: .85,
+      flux: 0.4,
+      reactnative: 0.5,
+      vuenative: 0.6,
+      html: 0.9,
+      javascript: 0.68,
+      motores: 0.9
+    },
+    meta: { class: '#01a029' }
+  },
+]
+const frontendCaptions = {
+  react: 'React',
+  vue: 'Vue',
+  flux: 'Flux',
+  reactnative: 'React Native',
+  vuenative: 'Vue Native',
+  html: 'Html y CSS',
+  javascript:  'Javascript',
+  motores: 'Motores de html'
+}
+
+const devOps = [
+  {
+    data: {
+      docker: 0.6,
+      dokku: .4,
+      heroku: 0.5,
+      bash: 0.8
+    },
+    meta: { color: '#00b92e' }
+  },
+];
+const devOpsCaptions = {
+  docker: 'Docker',
+  dokku: 'Dokku',
+  heroku: 'Heroku',
+  bash: 'Bash'
+}
+
+
+export const data = [backend, frontend, devOps];
+export const captions= [backendCaptions, frontendCaptions, devOpsCaptions];
